35969fe43e5a315a09bfc7c95e5df56ccf7585b1
Saudahmed Khan <saudahmed.khan@SPI.COM>
2019-01-23 19:57:01 +0530

    socket works! Alhamdulillah

--- Public/js/app.js
+++ Public/js/app.js
@@ -26,7 +26,7 @@ var app = angular.module('app', [  ]);
 
   
 
-  app.controller('MainCtrl', function($scope, socket) {
+  app.controller('MainCtrl', function($scope,$http, socket) {
     $scope.games = [];
     $scope.players=[];
 
@@ -34,4 +34,38 @@ var app = angular.module('app', [  ]);
       var game = {gameId:`gam-${i}sar${i}-dede-343434343-dff${i}`,gameName:`${i}sardines${i}`,gamePic:'/images/pic.png',gamePlayers:0};
       $scope.games.push(game);
     }
-  });
+    $scope.joinGame = function(id){
+      socket.emit('joinGame', id);
+      socket.on('joined',function(data) {
+        $('#divStatus').html(data);
+      });
+      let playerid = `PLA-${id.split('-')[1]}-${Date.now()}-sff`;
+      var player = {playerId:playerid,playerName:$scope.playerName,playerTyping:'',playerPic:'/images/pic.png'};
+      $scope.players.push(player);
+      $('#hidGameId').val(id);
+      $('#hidPlayerId').val(playerid)
+      let g = {gameId:id,playerId:playerid,playerName:$scope.playerName};
+      $http.post('/join',JSON.stringify(g))
+      .then(function (result) {
+        
+        console.log(result);
+      },
+      function(error){
+          alert(error.statusText);
+          $scope.wait = false;
+      });
+    }
+
+    $scope.typing = function(){
+      socket.emit('typing', `${$('#hidGameId').val()}~${$('#hidPlayerId').val()}~name`);
+      socket.on('onTyping',function(data){
+        if($('#hidGameId').val() == data[0]){
+          $.each($scope.players,function(i,v){
+            if(v.playerId == data[1]){
+              v.playerTyping = `typing ${data[2]}`
+            }
+          });
+        }
+      })
+    }
+});
--- index.js
+++ index.js
@@ -4,10 +4,23 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.json());
 
 io.sockets.on('connection', function(socket) {
 	console.log('someone connected');
+	socket.on('joinGame', function(room) { // is game id
+		socket.join(`room-${room}`);
+	});
+	socket.on('typing', function(val) { 
+		val = val.split('~');
+		io.sockets.in(`room-${val[0]}`).emit('onTyping', val);
+	});
 });
+app.post('/join',(req,res)=>{
+	let g = req.body
+	io.sockets.in(`room-${g.gameId}`).emit('joined', `${g.playerName} has joined...`);
+	return res.send(req.body);
+})
 
 server.listen(9000, function() {
   console.log('server up and running at 9000 port');
